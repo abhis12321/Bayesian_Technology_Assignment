88e0dc005de9ced0613a00bb0867ce1a2dd6f7cf
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-15 09:30:08 +0530

    a new component named searchForm is created

--- frontend-part/app/page.js
+++ frontend-part/app/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import SearchForm from './__components/SearchForm'
 
 export default function page() {
   return (
     <div className=''>
       hello project
+
+      <SearchForm />
     </div>
   )
 }
