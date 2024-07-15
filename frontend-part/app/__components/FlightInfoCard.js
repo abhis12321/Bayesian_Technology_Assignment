7f0524191f3b156d21378c16ad687d7cd46096f9
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-15 16:57:15 +0530

    FlightInfo is rendered with fetched data

--- frontend-part/app/__components/FlightInfoCard.js
+++ frontend-part/app/__components/FlightInfoCard.js
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
-export default function FlightInfoCard() {
+export default function FlightInfoCard({data}) {
+    console.log(data);
   return (
-    <div className=''>
-      Flight-Info-Card
+    <div className='flex flex-col gap-6 items-center justify-center rounded-xl'>
+        <Image src={'/logo.png'} alt='logo' width={20} height={20} />
     </div>
   )
 }
--- frontend-part/app/page.js
+++ frontend-part/app/page.js
@@ -1,15 +1,23 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import SearchForm from './__components/SearchForm'
+import FlightInfoCard from './__components/FlightInfoCard';
 
 export default function page() {  
+  const [flightData , setFlightData] = useState([]);
   const renderSearchData = (data) => {
+    setFlightData(data);
     console.log(data);
   }
 
   return (
     <div className=''>
       <SearchForm renderSearchData={renderSearchData}/>
+
+      {
+        flightData.map((data , index) => <FlightInfoCard key={index} data={data}/>)
+      }
+
     </div>
   )
 }
